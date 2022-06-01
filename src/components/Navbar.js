2838f51574ce1ef2336ed310593b76d621b911ee
DelhinRharl <delhinrharl@gmail.com>
2022-06-01 12:40:07 +0100

    Add Navbar components

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <img src="i.png" alt="img" />
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Experience</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
