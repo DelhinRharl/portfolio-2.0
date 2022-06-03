b60326696c80336eb779f3676a8175f6176b9ca0
DelhinRharl <delhinrharl@gmail.com>
2022-06-03 12:59:53 +0100

    Add the footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <p>&copy Affaxed. All Rights Reserved</p>
+        <p>2022</p>
+    </div>
+  )
+}
+
+export default Footer
