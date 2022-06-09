b182f4b5ede3b1d666e32550ee0d8d8d75dfcc55
DelhinRharl <delhinrharl@gmail.com>
2022-06-09 09:29:28 +0100

    Add Project code

---
+++ src/components/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Projects = () => (
+  <div>
+    <p> Here is a list of some of my projects</p>
+    <ul>
+      <li>Music Festival</li>
+      <li>Car Booking Service </li>
+      <li>Blog App</li>
+    </ul>
+
+  </div>
+);
+
+export default Projects;
