5be31fae6d80de4cc803edd4903273ecb23e2355
DelhinRharl <delhinrharl@gmail.com>
2022-06-09 09:27:55 +0100

    Add About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,50 @@
 import React from 'react';
 
-const Main = () => (
-  <div />
+const About = () => (
+  <div>
+    <h2>
+      <p>About Me</p>
+    </h2>
+    <h3>
+      Hello there. I am Affaxed Kiprotich
+    </h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus debitis cupiditate perspiciatis doloribus. Sit quod delectus ratione voluptate fugiat fuga, eum, facere omnis ullam neque dignissimos aliquam et quos quaerat!
+    </p>
+    <div>
+      <ul>
+        <li>
+          <p>
+            <strong>Web Development</strong>
+          </p>
+          <p>
+            I am a full stack web developer with a passion for building beautiful, responsive websites.
+          </p>
+        </li>
+        <li>
+          <p>
+            {' '}
+            <strong>Web Design</strong>
+            {' '}
+          </p>
+          <p>
+            I can create a website that is responsive and looks great on all devices.
+          </p>
+        </li>
+        <li>
+          <p>
+            {' '}
+            <strong>UI/UX Designer</strong>
+            {' '}
+          </p>
+          <p>
+            I can desingn the best looking UI/UX for your website.
+          </p>
+        </li>
+        <li />
+      </ul>
+    </div>
+  </div>
 );
 
-export default Main;
+export default About;
