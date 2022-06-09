dc9648a1f401df1a594ba4b7dd6eb3054c3506e8
DelhinRharl <delhinrharl@gmail.com>
2022-06-09 09:28:30 +0100

    Add footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-const Footer = () => {
-  return (
-    <div>
-        <p>&copy Affaxed. All Rights Reserved</p>
-        <p>2022</p>
-    </div>
-  )
-}
+const Footer = () => (
+  <div>
+    <p>&copy; Affaxed. All Rights Reserved</p>
+    <p>2022 </p>
+  </div>
+);
 
-export default Footer
+export default Footer;
