4ac8dbc708ef3a66c04c70873418118a760b1806
DelhinRharl <delhinrharl@gmail.com>
2022-06-09 09:30:39 +0100

    Add All components to the App.js compononent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import Experience from './components/Experience';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Services from './components/Services';
+import Projects from './components/Projects';
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Hero />
+    <About />
+    <Experience />
+    <Services />
+    <Projects />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
