8aac55161857630869161fa2042559d2483b9c6f
DelhinRharl <delhinrharl@gmail.com>
2022-06-13 09:07:00 +0100

    Fix route error in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Hero from './pages/Hero';
 import About from './pages/About';
 import Services from './pages/Services';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import Experience from './pages/Experience';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => (
   <div>
-    <Routes>
-      <Route path="/" element={<Hero />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/contacts" element={<Contact />} />
-    </Routes>
+    <BrowserRouter>
+        <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contacts" element={<Contact />} />
+        <Route path="/experience" element={<Experience />} />
+      </Routes>
+         <Footer />
+    </BrowserRouter>
   </div>
 );
 
