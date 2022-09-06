3ea7bbed50ed3b09c699ade815320b3438968df3
DelhinRharl <delhinrharl@gmail.com>
2022-09-06 15:41:11 +0100

    Add social media links

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -6,15 +6,18 @@ const Hero = () => (
     <div className="hero">
       <div className="hero-text">
         <h1>Hi, I am Affaxed Kiprotich</h1>
-        <p>I {''}
-        <Typed 
-          strings={[
-           'am from Kenya',
-            'build dynamic and responsive websites to suit your needs',
-          ]}
-          typeSpeed={50}
-          backSpeed={60}
-        />
+        <p>
+          I
+          {' '}
+
+          <Typed
+            strings={[
+              'am from Kenya.',
+              'will help you build dynamic and responsive websites to suit your needs...',
+            ]}
+            typeSpeed={50}
+            backSpeed={60}
+          />
         </p>
       </div>
       <div className="hero-image">
@@ -25,6 +28,26 @@ const Hero = () => (
       <a href="https://github.com/DelhinRharl/Portfolio">
         <button type="button">Portfolio</button>
       </a>
+    </div>
+    <div className="socials">
+      <h5>Socials</h5>
+      {/* <ul>
+        <li>
+          <a href="https://www.facebook.com/affaxed.kiprotich.3">
+            <i className="fab fa-facebook-f" />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/AffaxedK">
+            <i className="fab fa-twitter" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/affaxed_kiprotich/">
+            <i className="fab fa-instagram" />
+          </a>
+        </li>
+      </ul> */}
     </div>
   </div>
 );
