50f27bb842b93fff907b1e4d12e1d6fc45fdeb98
DelhinRharl <delhinrharl@gmail.com>
2022-09-06 13:03:08 +0100

    Edit hero page

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Hero = () => (
   <div className="hero-main">
-  <div className="hero">
-    <div className="hero-text">
-      <h3> Hi,I am Affaxed </h3>
-      <h1>I am a full stack developer based in Kenya</h1>
+    <div className="hero">
+      <div className="hero-text">
+        <h2>Hi, I am Affax</h2>
+        <h1>I am a full stack developer from Kenya</h1>
+        <p>I build user-friendly and dynamic web applications that suit your needs</p>
+      </div>
+      
+      <div className="hero-image">
+        <img src="" alt="iammsdjhfb" />
+      </div>
     </div>
-       <div className="hero-image">
-      <img src="" alt="iammsdjhfb" />
-    </div>
-  </div>
-  <div>
-        <a href="https://github.com/DelhinRharl/Portfolio">
+    <div>
+      <a href="https://github.com/DelhinRharl/Portfolio">
         <button type="button">Portfolio</button>
       </a>
     </div>
