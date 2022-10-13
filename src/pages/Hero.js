7f09ed07530ba78ba828f64cfe584f7c18e8b574
DelhinRharl <delhinrharl@gmail.com>
2022-10-13 15:14:57 +0100

    restructure the hero section

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,56 +1,28 @@
 import React from 'react';
 import Typed from 'react-typed';
-import img from '../images/g.avif'
+import img from '../images/g.avif';
+import About from './About';
 
 const Hero = () => (
-  <div className="hero-main">
     <div className="hero">
       <div className="hero-text">
-        <h1>Hi, I am Affaxed Kiprotich</h1>
+        <p>Hi, I am </p>
+        <h1>
+          Affaxed Kiprotich.
+        </h1>
+        <h2>
+          Here to help you build the best web applications
+        </h2>
         <p>
-          I
-          {' '}
-
-          <Typed
-            strings={[
-              'am from Kenya.',
-              'will help you build dynamic and responsive websites to suit your needs...',
-            ]}
-            typeSpeed={50}
-            backSpeed={60}
-          />
+          I specialize in full-stack web development, UI/UX design as well as optimizing your websites to run smoothly
         </p>
-          <div>
-      <a href="https://github.com/DelhinRharl/Portfolio">
-        <button type="button">Portfolio</button>
-      </a>
-    </div>
-      </div>
-      <div className="hero-image">
-        <img src={img} alt="iammsdjhfb" />
+        <div>
+          <a href="https://github.com/DelhinRharl">
+            <button type="button">GitHub</button>
+          </a>
+        </div>
       </div>
     </div>
-    <div className="socials">
-      <h5>Socials</h5>
-      {/* <ul>
-        <li>
-          <a href="https://www.facebook.com/affaxed.kiprotich.3">
-            <i className="fab fa-facebook-f" />
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/AffaxedK">
-            <i className="fab fa-twitter" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/affaxed_kiprotich/">
-            <i className="fab fa-instagram" />
-          </a>
-        </li>
-      </ul> */}
-    </div>
-  </div>
 );
 
 export default Hero;
