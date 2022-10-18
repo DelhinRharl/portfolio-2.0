2c0ebf6c0a1ede3e4da521ccd43dbe669183ec54
DelhinRharl <delhinrharl@gmail.com>
2022-10-18 23:45:39 +0100

    style the edited pages

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const Socials = () => {
-  return (
-    <div>Socials</div>
-  )
-}
+const Socials = () => (
+  <div>Socials</div>
+);
 
-export default Socials
+export default Socials;
