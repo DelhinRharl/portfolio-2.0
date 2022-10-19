9b09237c05bbab339d388ff56ae1678cdabf5970
DelhinRharl <delhinrharl@gmail.com>
2022-10-19 20:37:14 +0100

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Hero from './pages/Hero';
 import About from './pages/About';
 import Services from './pages/Services';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-import Experience from './pages/Experience';
+// import Experience from './pages/Experience';
 import Navbar from './components/Navbar';
 // import Footer from './components/Footer';
 // import Name from './components/Name';
@@ -14,18 +14,19 @@ const App = () => (
   <div className="font-body bg-black bg-cover be bg-containt text-white">
     <BrowserRouter>
       <Navbar />
-      {/* <About/> */}
-      {/* <Services />
+      <Hero/>
+      <About/>
+      <Services />
       <Projects />
-      <Contact /> */}
-      <Routes>
+      <Contact />
+      {/* <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contacts" element={<Contact />} />
         <Route path="/experience" element={<Experience />} />
-      </Routes>
+      </Routes> */}
       {/* <Navbar /> */}
       {/* <Footer /> */}
     </BrowserRouter>
