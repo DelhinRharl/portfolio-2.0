f45a42f5d563e924494030cd871ef23d04ce76a4
DelhinRharl <delhinrharl@gmail.com>
2022-10-19 20:37:32 +0100

    edit about page for minor changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,15 +5,12 @@ const longText1 = 'Hi, My name is Affaxed Kiprotich. I am a web developer from K
 const longText2 = 'I am a graduate of Microverse, a remote software development school. I have been learning for the past 1 year and I am still learning. I have worked on several projects, both solo and collaborative; that vary in complexity and currently looking for opportunities to work on more challenging projects.';
 
 const About = () => (
-  <div className="pl-20 pt-10 gap-7 flex flex-col items-center  h-[84vh] text-xl">
-    <h1 className="text-4xl font-bold text-amber-600">
+  <div className="about">
+    <h1 className="">
       {' '}
       About Me
-      <p className="w-10 h-10 bg-amber-600 rounded-full  absolute top-24  left-17 -z-10 " />
+      <p className="" />
     </h1>
-    <p className="text-2xl font-extrabold">
-      I am Affaxed Kiprotich
-    </p>
     <p>
       {longText1}
     </p>
