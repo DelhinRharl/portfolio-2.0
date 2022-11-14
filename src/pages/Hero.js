1b7c4b41e1ef3fe89aedd586a4abd1fb7c7dda60
DelhinRharl <delhinrharl@gmail.com>
2022-11-14 20:53:34 +0000

    Edit hero page

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,28 +1,27 @@
 import React from 'react';
 
+const longText1 = 'I create styish and sophisticated websites to suit your needs using the latest technologies; as well as offer other web related services.';
+
 const Hero = () => (
   <div className="hero">
     <div className="hero-text">
-      <p>Hi, I am </p>
-      <h1>
-        Affaxed Kiprotich
-      </h1>
-      <h2>
-        I build web applications
-      </h2>
+      <p> </p>
+      <h3>
+        Web Developer
+      </h3>
       <p>
-        I specialize in full-stack web development, UI/UX design
+        {longText1}
       </p>
-      <p>
-        as well as optimizing your websites to run smoothly
-      </p>
-      <div className="hero-btns">
+      {/* <div className="hero-btns">
         <a href="https://github.com/DelhinRharl">
           <button type="button">Find out more!</button>
         </a>
         <a href="https://docs.google.com/document/d/19pVS_0LzVRiFRqxSuZgcpXu9yhSraHKdkBwn-9uJtHo/edit?usp=sharing">
           <button type="button">Resume</button>
         </a>
+      </div> */}
+      <div>
+        <p>Affaxed Kiprotich</p>
       </div>
     </div>
   </div>
