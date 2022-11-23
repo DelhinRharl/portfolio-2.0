144e2b99de71332734923adb19a0fc8f93bdfc7b
DelhinRharl <delhinrharl@gmail.com>
2022-11-23 08:15:41 +0000

    Edit photo component

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Photo = () => (
-  <div  className ="photo">
+  <div className="photo">
     <h1>
-        jksfsd f
+      jksfsd f
     </h1>
-    <img src="https://avatars.githubusercontent.com/u/60448833?v=4" alt="Affaxed Kiprotich" />
+    <img src="" alt="Affaxed Kiprotich" />
   </div>
 );
 
