5934a4f66fe288e63bec893567fe08b1e8acc10f
DelhinRharl <delhinrharl@gmail.com>
2022-11-23 07:45:42 +0000

    Edit photo component

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-const Photo = () => {
-  return (
-    <div>
-        <img src="https://avatars.githubusercontent.com/u/60448833?v=4" alt="Affaxed Kiprotich" />
-    </div>
-  )
-}
+const Photo = () => (
+  <div  className ="photo">
+    <h1>
+        jksfsd f
+    </h1>
+    <img src="https://avatars.githubusercontent.com/u/60448833?v=4" alt="Affaxed Kiprotich" />
+  </div>
+);
 
-export default Photo
+export default Photo;
