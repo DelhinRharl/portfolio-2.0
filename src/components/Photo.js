ebd7aab18e5d7983778f1636b6d21814fd6e1af4
DelhinRharl <delhinrharl@gmail.com>
2022-11-23 07:17:39 +0000

    Add new component

---
+++ src/components/Photo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Photo = () => {
+  return (
+    <div>
+        <img src="https://avatars.githubusercontent.com/u/60448833?v=4" alt="Affaxed Kiprotich" />
+    </div>
+  )
+}
+
+export default Photo
