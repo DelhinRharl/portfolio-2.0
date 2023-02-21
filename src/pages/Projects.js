92a2e07efdd57c98324b74ce9d930462e6f85b7b
DelhinRharl <delhinrharl@gmail.com>
2023-02-21 22:12:39 +0000

    Fetch data from github api

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,34 @@
-import React, {useState, use} from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Projects = () => (
-  <section className="projects">
-    <h2>Projects</h2>
-  </section>
-);
+function Projects() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        const response = await fetch('https://api.github.com/users/DelhinRharl/repos');
+        const data = await response.json();
+        const selectedRepos = data.filter((repo) => repo.name === 'car-booking-service-front-end' || repo.name === 'Capstone-Project-Affaxed' || repo.name === 'blog');
+        setRepos(selectedRepos);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchRepos();
+  }, []);
+
+  return (
+    <div>
+      <h2>Projects</h2>
+      {repos.map((repo) => (
+        <div key={repo.id}>
+          <h3>{repo.name}</h3>
+          <p>{repo.description}</p>
+          <a href={repo.html_url}>View Repo</a>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default Projects;
