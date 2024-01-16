58f976ff0dd55cd4a4c8d02404718f6cc1c1a5ab
DelhinRharl <delhinrharl@gmail.com>
2024-01-16 21:54:42 +0000

    Remove navbar

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Hero from './pages/Hero';
 import About from './pages/About';
 import Services from './pages/Services';
 import Projects from './pages/Projects';
-import Navbar from './components/Navbar';
-import Photo from './components/Photo';
+// import Navbar from './components/Navbar';
+// import Photo from './components/Photo';
 import Sidebar from './components/Sidebar';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -13,9 +13,9 @@ import Footer from './components/Footer';
 const App = () => (
   <div>
     <BrowserRouter>
-      <Navbar />
+      {/* <Navbar /> */}
       <Sidebar />
-      <Photo />
+      {/* <Photo /> */}
       <Hero />
       <About />
       <Services />
